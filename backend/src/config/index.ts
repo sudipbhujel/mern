1b13575a5ecdd0d56746ed96117a2b7cb9636ee4
Sudip Bhujel <devsudipbhujel@gmail.com>
2021-11-24 16:16:28 +0545

    fix(whitelist): update config whitelist

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,3 +1,3 @@
 export default {
-  whitelist: ["http://localhost:4000", "http://localhost:3000", "api.sudipbhujel.ga"],
+  whitelist: ["http://localhost:4000", "http://localhost:3000", "https://api.sudipbhujel.ga"],
 }
